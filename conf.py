6a2f5b7da8b493a192f60f42a8b2ef3970bf24a9
腾飞 张 <ZTFword@outlook.com>
2021-02-22 21:12:25 +0800

    主页修改

--- conf.py
+++ conf.py
@@ -14,19 +14,19 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
+    "enabled": True,
     "repo": ""
 }
 
 # 站点设置
-site_name = "穿山甲老窝"
+site_name = "穿山甲小窝"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "穿山甲叔叔"
-email = "hi@imalan.cn"
+email = "ztfword@foxmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "回收废旧大老婆二老婆"
+key_words = ['Maverick', '穿山甲叔叔', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
@@ -35,9 +35,9 @@ external_links = [
         "brief": "🏄‍ Go My Own Way."
     },
     {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "name": "Lei咯",
+        "url": "https://1244919208.github.io/Blog-m/",
+        "brief": "穿山甲叔叔的主页。"
     }
 ]
 nav = [
@@ -61,17 +61,17 @@ nav = [
 social_links = [
     {
         "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
+        "url": "https://twitter.com/2233word",
         "icon": "gi gi-twitter"
     },
     {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/1244919208",
         "icon": "gi gi-github"
     },
     {
         "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
+        "url": "https://weibo.com/u/6774943340/home",
         "icon": "gi gi-weibo"
     }
 ]
