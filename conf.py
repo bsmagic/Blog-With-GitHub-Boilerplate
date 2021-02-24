2be4f2bc3e5d5d4730f32b86cca8e29bb8c930dd
腾飞 张 <ZTFword@outlook.com>
2021-02-24 15:45:24 +0800

    Update site

--- conf.py
+++ conf.py
@@ -103,6 +103,15 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 </script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5a13125bbc19997dac17447d8a30bee5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 
 '''
 
