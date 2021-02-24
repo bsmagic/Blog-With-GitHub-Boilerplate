ddadc0f56aec4c391bede9534ccda6ff4ae62191
腾飞 张 <ZTFword@outlook.com>
2021-02-24 15:47:56 +0800

    Update site

--- conf.py
+++ conf.py
@@ -90,6 +90,17 @@ social_links = [
 head_addon = r'''
 <meta http-equiv="x-dns-prefetch-control" content="on">
 <link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
+''',
+'''
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5a13125bbc19997dac17447d8a30bee5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 
 '''
 
@@ -103,16 +114,6 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 </script>
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?5a13125bbc19997dac17447d8a30bee5";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
-
 '''
 
 body_addon = ''
